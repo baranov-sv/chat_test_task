acf969882b507e12e98de609ebb648afc4829873
Sergey Baranov <sergey@convead.com>
2022-04-05 10:45:38 +0300

    Message::Create : lock chat record before update cache columns

--- app/services/messages/create.rb
+++ app/services/messages/create.rb
@@ -1,6 +1,7 @@
 module Messages
   class Create
     prepend ServiceModule::Base
+    MAX_RETRY = 1
 
     def call(user:, chat:, text:, attachment: nil)
       message = Message.new(
@@ -11,29 +12,39 @@ module Messages
         attachment: attachment
       )
             
-      ApplicationRecord.transaction do
-        message.save!
-        # изменяем кеш поля у чата
-        chat.update_columns(
-          last_message_at: message.sent_at,
-          last_message_text: message.text,
-          last_message_sender_id: user.id
-        )
-        # добавляем непрочитанное сообщение всем участникам чата кроме текущего пользователя
-        raw_sql = <<RAW
-          unread_messages = unread_messages + 1,
-          unread_message_ids = array_append(unread_message_ids,  :message_id)
+      retry_count = 0
+      begin
+        chat.with_lock('FOR UPDATE NOWAIT') do
+          message.save!
+          # изменяем кеш поля у чата
+          chat.update_columns(
+            last_message_at: message.sent_at,
+            last_message_text: message.text,
+            last_message_sender_id: user.id
+          )
+          # добавляем непрочитанное сообщение всем участникам чата кроме текущего пользователя
+          raw_sql = <<RAW
+            unread_messages = unread_messages + 1,
+            unread_message_ids = array_append(unread_message_ids,  :message_id)
 RAW
-        update_scope = chat.participants.where.not(user_id: user.id)
+          update_scope = chat.participants.where.not(user_id: user.id)
 
-        update_scope.update_all([raw_sql, message_id: message.id])
+          update_scope.update_all([raw_sql, message_id: message.id])
+        end
+
+        # для текущего пользователя помечаем сообщение как прочитанное
+        message.read!
+        success(message)
+      rescue ActiveRecord::LockWaitTimeout
+        if retry_count < MAX_RETRY
+         retry_count += 1
+         retry
+        else
+         raise
+        end
+      rescue ActiveRecord::Rollback, ActiveRecord::RecordInvalid
+        failure(message)
       end
-
-      # для текущего пользователя помечаем сообщение как прочитанное
-      message.read!
-      success(message)
-    rescue ActiveRecord::Rollback, ActiveRecord::RecordInvalid
-      failure(message)
     end
   end
 end
